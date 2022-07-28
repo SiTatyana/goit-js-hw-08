94119152215ce0a212416bd450b023043ee46784
SiTatyana <sisenorawork@gmail.com>
2022-07-28 18:27:10 +0100

    make changes in task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,52 @@
 import throttle from 'lodash.throttle';
 
-// const throttle = require('lodash.throttle');
-
 const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('input'),
     message: document.querySelector('textarea'),
 };
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onFormInput, 500));
-
-const STORAGE_KEY = "feedback-form-state";
-
+let STORAGE_KEY = 'feedback-form-state';
 processingTheForm();
 
-const formData = {};
 
-function onFormSubmit(e) {
+
+// refs.form.addEventListener('submit', (onFormSubmit));
+refs.form.addEventListener('submit', e  => {
   e.preventDefault();
-  formData.email = refs.email.value;
-  formData.message = refs.message.value;
-  console.log(formData);
+  const formData = new FormData(filterForm);
+  formData.forEach((value, name) => console.log(value, name));
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-};
+});
 
-
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 function onFormInput(e) {
+  let formData = localStorage.getItem(STORAGE_KEY);
+  formData = formData ? JSON.parse(formData) : {};
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
+
+
+// const formData = {};
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   formData.email = refs.email.value;
+//   formData.message = refs.message.value;
+//   console.log(formData);
+//   e.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// };
+
+
+// function onFormInput(e) {
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// };
+
+
 
 function processingTheForm() {
     const formValues = localStorage.getItem(STORAGE_KEY);
