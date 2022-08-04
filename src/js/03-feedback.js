6e145f31b92caba1a2e3ab71346007ecfb5f4be6
SiTatyana <sisenorawork@gmail.com>
2022-08-04 15:24:41 +0100

    +""

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,22 +12,25 @@ const STORAGE_KEY = "feedback-form-state";
 
 processingTheForm();
 
-const formData = {email:"", name:""};
+
+// const formData = {email:"", name:""};
+let formData = {};
 
 function onFormSubmit(e) {
-    e.preventDefault();
-  formData.email = refs.email.value;
-  formData.message = refs.message.value;
-  console.log(formData);
+  e.preventDefault();
   e.currentTarget.reset();
+  formData.email = refs.email.value || "";
+  formData.message = refs.message.value || "";
+  console.log(formData);
   localStorage.removeItem(STORAGE_KEY);
+  
 };
 
 function onFormInput(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
-
+        
 function processingTheForm() {
   const formValues = localStorage.getItem(STORAGE_KEY);
   const objectValues = JSON.parse(formValues);
@@ -37,39 +40,7 @@ function processingTheForm() {
     refs.email.value = saveEmail;
     const saveMessage = objectValues.message;
     refs.message.value = saveMessage;
-    
+   
   };
 };
 
-
-// let STORAGE_KEY = 'feedback-form-state';
-// processingTheForm();
-
-// refs.form.addEventListener('submit', e  => {
-//   e.preventDefault();
-//   const formData = new FormData(refs.form);
-//   formData.forEach((value, name) => console.log(value, name));
-//   e.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// });
-
-// refs.form.addEventListener('input', throttle(onFormInput, 500));
-// function onFormInput(e) {
-//   let formData = localStorage.getItem(STORAGE_KEY);
-//   formData = formData ? JSON.parse(formData) : {};
-//   formData[e.target.name] = e.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// };
-
-// function processingTheForm() {
-//     const formValues = localStorage.getItem(STORAGE_KEY);
-//     const objectValues = JSON.parse(formValues);
-   
-//     if (objectValues) {
-//         const saveEmail = objectValues.email;
-//         refs.email.value = saveEmail;
-
-//         const saveMessage = objectValues.message;
-//         refs.message.value = saveMessage;
-//     };
-// };
